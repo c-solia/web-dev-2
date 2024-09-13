0da606719afdfcb9270aa403ba81123b829a74bb
Cl4mage <164264759+Cl4mage@users.noreply.github.com>
2024-09-13 12:45:11 +1000

    5 - Added Search Page Form, Fetch. Added API 3 for search page

--- fundraiserAPI/controllerAPI/api-controller.js
+++ fundraiserAPI/controllerAPI/api-controller.js
@@ -36,11 +36,36 @@ router.get("/category",(req,res) =>{
 })
 
 //Get request 3
-/**
- * 
- * ADD GET REQUEST HERE
- * 
- */
+router.get("/search",(req,res) =>{
+
+    const category = req.query.category;
+    const city = req.query.city;
+    const organiser = req.query.organiser;
+
+    let query = `SELECT FUNDRAISER.FUNDRAISER_ID, FUNDRAISER.ORGANIZER, FUNDRAISER.CAPTION, FUNDRAISER.TARGET_FUNDING, FUNDRAISER.CURRENT_FUNDING, FUNDRAISER.CITY, FUNDRAISER.ACTIVE, FUNDRAISER.IMG_URL, CATEGORY.NAME AS CATEGORY_NAME 
+        FROM FUNDRAISER INNER JOIN CATEGORY ON FUNDRAISER.CATEGORY_ID = CATEGORY.CATEGORY_ID WHERE FUNDRAISER.ACTIVE = TRUE`
+
+        if (category != '') {
+            query += ` AND CATEGORY.NAME = '${category}'`;
+        }
+        if (city != '') {
+            query += ` AND FUNDRAISER.CITY = '${city}'`;
+        }
+        if (organiser != '') {
+            query += ` AND FUNDRAISER.ORGANIZER = '${organiser}'`;
+        }
+
+    connection.query(query,(err,records, fields) =>{
+    if(err){
+        console.log("Error while retrieving fundraisers", err);
+        console.log("cat = " + category + ", city = "+ city + "  , org = "+ organiser);
+        console.log(req.query);
+    }
+    else {
+        res.send(records);
+    }
+    })
+})
 
 
 //Get request 4
